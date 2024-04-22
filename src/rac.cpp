82c7ec94a775a93087e67b608658641ee48995a8
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-04-22 14:08:24 -0400

    Started adding vector3's

--- src/rac.cpp
+++ src/rac.cpp
@@ -160,7 +160,6 @@ namespace rac
                 return u64_state = x;
             }
 
-            /*
             mut_i64 i64_state = 1680588011350901LL;
             static i64 GetI64()
             {
@@ -170,7 +169,6 @@ namespace rac
                 x ^= x << 11;
                 return i64_state = x;
             }
-            */
         }
     }
 
@@ -892,14 +890,14 @@ namespace rac
             return v2(lhs.x / rhs.x, lhs.y / rhs.y);
         }
 
-        const v2 ZERO = v2(0.0f, 0.0f);
-        const v2 ONE = v2(1.0f, 1.0f);
-        const v2 SIGNED_ONE = -ONE;
+        const v2 V2_ZERO = v2(0.0f, 0.0f);
+        const v2 V2_ONE = v2(1.0f, 1.0f);
+        const v2 V2_SIGNED_ONE = -V2_ONE;
 
-        const v2 RIGHT = v2(1.0f, 0.0f);
-        const v2 LEFT = -RIGHT;
-        const v2 UP = v2(0.0f, 1.0f);
-        const v2 DOWN = -UP;
+        const v2 V2_RIGHT = v2(1.0f, 0.0f);
+        const v2 V2_LEFT = -V2_RIGHT;
+        const v2 V2_UP = v2(0.0f, 1.0f);
+        const v2 V2_DOWN = -V2_UP;
 
         // Check if two vectors are approximately equal
         static MAY_INLINE bool Approx(v2_ref a, v2_ref b)
@@ -928,7 +926,7 @@ namespace rac
 
         static MAY_INLINE v2 Refract(v2_ref incoming, v2_ref normal, f32 refractRatio)
         {
-            v2 res = ZERO;
+            v2 res = V2_ZERO;
             f32 dot = incoming.Dot(normal);
             mut_f32 d = 1.0f - refractRatio * refractRatio * (1.0f - dot*dot);
             if (d >= F32_EPSILON)
@@ -940,6 +938,305 @@ namespace rac
 
             return res;
         }
+
+        class v3;
+        typedef const v3* v3_ptr;   typedef const v3& v3_ref;
+        typedef v3* mut_v3_ptr; typedef v3& mut_v3_ref;
+
+        i32 V3_STRING_MAX = (F32_STR_LEN * 3) + (COMMA_SPACE_LEN * 2) + PARENTHESES_LEN + 1;
+        i32 V3_STRING_LEN = V3_STRING_MAX - 1;
+        class v3
+        {
+        public:
+            mut_f32 x = 0.0f;
+            mut_f32 y = 0.0f;
+            mut_f32 z = 0.0f;
+
+            v3() { }
+
+            v3(i8 _x, i8 _y, i8 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+            v3(i16 _x, i16 _y, i16 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+            v3(i32 _x, i32 _y, i32 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+            v3(u8 _x, u8 _y, u8 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+            v3(u16 _x, u16 _y, u16 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+            v3(u32 _x, u32 _y, u32 _z)
+            {
+                x = (mut_f32)_x;
+                y = (mut_f32)_y;
+                z = (mut_f32)_z;
+            }
+
+            v3(f32 _a)
+            {
+                x = _a;
+                y = _a;
+                z = _a;
+            }
+            v3(f32 _x, f32 _y)
+            {
+                x = _x;
+                y = _y;
+                z = 0.0f;
+            }
+            v3(f32 _x, f32 _y, f32 _z)
+            {
+                x = _x;
+                y = _y;
+                z = _z;
+            }
+            v3(v2 v, f32 _z)
+            {
+                x = v.x;
+                y = v.y;
+                z = _z;
+            }
+            v3(v2 v)
+            {
+                x = v.x;
+                y = v.y;
+                z = 0.0f;
+            }
+
+            INLINE v3_ref operator=(v3_ref rhs)
+            {
+                x = rhs.x;
+                y = rhs.y;
+                z = rhs.z;
+            }
+            INLINE v3 operator -() const { return v3(-x, -y, -z); }
+            INLINE v3 operator *(f32 v) { return v3(x * v, y * v, z * v); }
+            INLINE v3 operator /(f32 v)
+            {
+                f32 inv = 1.0f / v;
+                return v3(x * inv, y * inv, z * inv);
+            }
+            INLINE v3_ref operator +=(v3_ref rhs)
+            {
+                x += rhs.x;
+                y += rhs.y;
+                z += rhs.z;
+            }
+            INLINE v3_ref operator -=(v3_ref rhs)
+            {
+                x -= rhs.x;
+                y -= rhs.y;
+                z -= rhs.z;
+            }
+            INLINE v3_ref operator *=(v3_ref rhs)
+            {
+                x *= rhs.x;
+                y *= rhs.y;
+                z *= rhs.z;
+            }
+            INLINE v3_ref operator *=(f32 a)
+            {
+                x *= a;
+                y *= a;
+                z *= a;
+            }
+            INLINE v3_ref operator /=(v3_ref rhs)
+            {
+                x /= rhs.x;
+                y /= rhs.y;
+                z /= rhs.z;
+            }
+            INLINE v3_ref operator /=(f32 a)
+            {
+                f32 inv = 1.0f / a;
+                x *= inv;
+                y *= inv;
+                z *= inv;
+            }
+
+            INLINE ptr Ptr() const { return (ptr)(&x); }
+            INLINE v3_ref Ref() const { return *this; }
+
+            INLINE str_ref ToStr(mut_str_ref str) const
+            {
+                char buff[V3_STRING_MAX] = { 0 };
+                sprintf_s(buff, V3_STRING_LEN, "(%0.3f, %0.3f, %0.3f)", x, y, z);
+                return str = buff;
+            }
+
+            INLINE v3 Max(v3 v)
+            {
+                return v3(fmaxf(x, v.x), fmaxf(y, v.y), fmaxf(z, v.z));
+            }
+            INLINE v3 Min(v3 v)
+            {
+                return v3(fminf(x, v.x), fminf(y, v.y), fminf(z, v.z));
+            }
+            INLINE v3 Clamp(f32 min, f32 max)
+            {
+                return v3(  x > max ? max : (x < min ? min : x),
+                            y > max ? max : (y < min ? min : y),
+                            z > max ? max : (z < min ? min : z));
+            }
+            INLINE v3 Clamp(f32 x_min, f32 x_max, f32 y_min, f32 y_max, f32 z_min, f32 z_max)
+            {
+                return v3(  x > x_max ? x_max : (x < x_min ? x_min : x),
+                            y > y_max ? y_max : (y < y_min ? y_min : y),
+                            z > z_max ? z_max : (z < z_min ? z_min : z));
+            }
+            INLINE v3 Clamp(v3 min, v3 max)
+            {
+                return v3(  x > max.x ? max.x : (x < min.x ? min.x : x),
+                            y > max.y ? max.y : (y < min.y ? min.y : y),
+                            z > max.z ? max.z : (z < min.z ? min.z : z));
+            }
+            INLINE v3 Inv() const
+            {
+                return v3(1.0f / x, 1.0f / y, 1.0f / z);
+            }
+            INLINE f32 Mag() const
+            {
+                return sqrtf(x * x + y * y + z * z);
+            }
+            INLINE f32 InvMag() const
+            {
+                return 1.0f / sqrtf(x * x + y * y + z * z);
+            }
+            INLINE f32 SqrMag() const
+            {
+                return x * x + y * y + z * z;
+            }
+            INLINE f32 Dot(v3_ref v) const
+            {
+                return x * v.x + y * v.y + z * v.z;
+            }
+            INLINE f32 Dist(v3_ref from) const
+            {
+                return sqrtf((x - from.x) * (x - from.x) +
+                            (y * from.y) * (y * from.y) +
+                            (z * from.z) * (z * from.z));
+            }
+            INLINE f32 SqrDist(v3_ref from) const
+            {
+                return  (x - from.x) * (x - from.x) +
+                        (y * from.y) * (y * from.y) +
+                        (z * from.z) * (z * from.z);
+            }
+            INLINE v3 Norm() const
+            {
+                f32 inv_mag = InvMag();
+                if (inv_mag > F32_EPSILON)
+                {
+                    return v3(x * inv_mag, y * inv_mag, z * inv_mag);
+                }
+                else return *this;
+            }
+
+            // Cross product with unit X Vector3
+            // Unit X == v3(1.0f, 0.0f, 0.0f)
+            INLINE v3 CrossX() { return v3(0.0f, z, -y); }
+
+            // Cross product with unit Y Vector3
+            // Unit Y == v3(0.0f, 1.0f, 0.0f)
+            INLINE v3 CrossY() { return v3(-z, 0.0f, x); }
+
+            // Cross product with unit X Vector3
+            // Unit Z == v3(0.0f, 0.0f, 1.0f)
+            INLINE v3 CrossZ() { return v3(y, -x, 0.0f); }
+        };
+
+        INLINE static bool operator >(v3_ref lhs, v3_ref rhs)
+        {
+            return  lhs.x > rhs.x && lhs.y > rhs.y && lhs.z > rhs.z;
+        }
+        INLINE static bool operator <(v3_ref lhs, v3_ref rhs)
+        {
+            return lhs.x < rhs.x && lhs.y < rhs.y && lhs.z < rhs.z;
+        }
+        INLINE static bool operator >=(v3_ref lhs, v3_ref rhs)
+        {
+            return lhs.x >= rhs.x && lhs.y >= rhs.y && lhs.z >= rhs.z;
+        }
+        INLINE static bool operator <=(v3_ref lhs, v3_ref rhs)
+        {
+            return lhs.x <= rhs.x && lhs.y <= rhs.y && lhs.z <= rhs.z;
+        }
+        INLINE static bool operator ==(v3_ref lhs, v3_ref rhs)
+        {
+            return  lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
+        }
+        INLINE static bool operator !=(v3_ref lhs, v3_ref rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        INLINE static v3 operator +(v3_ref lhs, v3_ref rhs)
+        {
+            return v3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+        INLINE static v3 operator -(v3_ref lhs, v3_ref rhs)
+        {
+            return v3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        }
+        INLINE static v3 operator *(v3_ref lhs, v3_ref rhs)
+        {
+            return v3(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z);
+        }
+        INLINE static v3 operator /(v3_ref lhs, v3_ref rhs)
+        {
+            return v3(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z);
+        }
+
+        // Check if two vectors are approximately equal
+        static MAY_INLINE bool Approx(v3_ref a, v3_ref b)
+        {
+            return  RAC_F32_APPROX(a.x, b.x) &&
+                    RAC_F32_APPROX(a.y, b.y) &&
+                    RAC_F32_APPROX(a.z, b.z);
+        }
+        static MAY_INLINE bool NotApprox(v3_ref a, v3_ref b)
+        {
+            return !Approx(a, b);
+        }
+
+        static MAY_INLINE v3 Cross(v3 v1, v3 v2)
+        {
+            return v3(  v1.y * v2.z - v1.z * v2.y,
+                        v1.z * v2.x - v1.x * v2.z,
+                        v1.x * v2.y - v1.y * v2.x);
+        }
+
+        const v3 V3_ZERO = v3(0.0f);
+        const v3 V3_ONE = v3(1.0f);
+        const v3 V3_SIGNED_ONE = -V3_ONE;
+
+        const v3 V3_RIGHT = v3(1.0f, 0.0f, 0.0f);
+        const v3 V3_LEFT = -V3_RIGHT;
+
+        const v3 V3_UP = v3(0.0f, 1.0f, 0.0f);
+        const v3 V3_DOWN = -V3_UP;
+
+        const v3 V3_FORWARD = v3(0.0f, 0.0f, 1.0f);
+        const v3 V3_BACKWARD = -V3_FORWARD;
     }
 
     namespace collections
